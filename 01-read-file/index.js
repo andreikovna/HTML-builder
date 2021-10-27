01f06e2af2187286a862c4c1ea3a1e07c601bdd1
Daria Litvinova <d.andreikovna@gmail.com>
2021-10-27 22:45:22 +0300

    feat: add 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,26 @@
+// const { stdin, stdout, exit} = process;
+// const flag = process.argv[2];
+// const allowedFlags = ['-f', '-d'];
+// if (!allowedFlags.includes(flag)) {
+//     stdout.write('Попробуйте ещё раз запустить файл с флагом -f или -d');
+//     exit();
+// }
+// if (flag == '-d') {
+//     console.log(__dirname);
+// }
+// if (flag == '-f'){
+//     console.log(__filename);
+// }
+
+const path = require('path');
+const fs = require('fs');
+
+const resultText = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(resultText, 'utf-8');
+
+readStream.on ('data', chunk => console.log(chunk));
+
+
+
+
+
