1d1f8f453e1b574b0f26e5d58e88a24b9940d333
Daria Litvinova <d.andreikovna@gmail.com>
2021-10-28 22:47:41 +0300

    feat: add solution 03-files-in-folder

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,18 +5,18 @@ const {stdin, stdout} = process;
 const resultText = path.join(__dirname, 'text.txt');
 const output = fs.createWriteStream(resultText);
 
-console.log ('please, write smth here:');
+console.log ('\nplease, write smth here:');
 stdin.on('data', data => {
     const receivedData = data.toString().trim();
     if (receivedData == 'exit') {
         process.exit();
     } else {
         output.write(data); 
-        console.log('you can add smth or write "exit"')
+        console.log('\nyou can add smth or write "exit"')
     }  
 });
 
 process.on('SIGINT', function () {
     process.exit();
   });
-process.on('exit', () => stdout.write('good luck!'));
+process.on('exit', () => stdout.write('\ngood luck!'));
--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,8 +1,34 @@
-const {stdin, stdout} = process;
-console.log('what');
-stdin.on ('data', data => {
-    stdout.write(data);
-    process.exit();
-})
+const path = require('path');
+const fs = require('fs');
 
-process.on('exit', () => {stdout.write('goodbye')});
+const folder = path.join(__dirname, 'secret-folder');
+console.log (folder)
+
+fs.readdir(folder,  { withFileTypes: true }, (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("\nCurrent directory filenames:\n");
+
+      files.forEach(file => {
+
+          if (file.isFile()) {
+            const fileName = file.name.toString().split('.')[0];
+            const extName = file.name.toString().split('.')[1];
+            const filePath = path.join(__dirname, 'secret-folder', file.name)
+            fs.stat(filePath, (error, stats) => {
+                if (error) {
+                  console.log(error);
+                }
+                else {
+                  const fileSize = stats.size*0.001;
+                  console.log(`${fileName} - ${extName} - ${fileSize}kb`);
+                }
+              });
+            
+            
+          }
+        
+      })
+    }
+  })
