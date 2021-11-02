955e80a206f0b694bdada14384bfead50547fbfb
Daria Litvinova <d.andreikovna@gmail.com>
2021-11-02 11:59:30 +0300

    feat: add styles build to 06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -34,38 +34,38 @@ template.on('data', async (data) => {
 })
 
 
-// const resultStyles = path.join(__dirname, "project-dist", "style.css");
-// const sourseStyles = path.join(__dirname, "styles");
-// const outputStyles = fs.createWriteStream(resultStyles);
+const resultStyles = path.join(__dirname, "project-dist", "style.css");
+const sourseStyles = path.join(__dirname, "styles");
+const outputStyles = fs.createWriteStream(resultStyles);
 
-// fs.readdir(sourseStyles, { withFileTypes: true }, (err, files) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     files.forEach((file) => {
-//       if (file.isFile()) {
-//         const fileName = file.name.toString();
-//         const extName = file.name.toString().split(".")[1];
-//         if (extName == "css") {
-//           fs.readFile(
-//             path.join(__dirname, "styles", fileName),
-//             "utf-8",
-//             (err, data) => {
-//               if (err) throw err;
-//               const mergedStyles = [];
-//               const style = data.toString();
-//               mergedStyles.push(style);
+fs.readdir(sourseStyles, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach((file) => {
+      if (file.isFile()) {
+        const fileName = file.name.toString();
+        const extName = file.name.toString().split(".")[1];
+        if (extName == "css") {
+          fs.readFile(
+            path.join(__dirname, "styles", fileName),
+            "utf-8",
+            (err, data) => {
+              if (err) throw err;
+              const mergedStyles = [];
+              const style = data.toString();
+              mergedStyles.push(style);
 
-//               for (let i = 0; i < mergedStyles.length; i++) {
-//                 outputStyles.write(mergedStyles[i]);
-//               }
-//             }
-//           );
-//         }
-//       }
-//     });
-//   }
-// });
+              for (let i = 0; i < mergedStyles.length; i++) {
+                outputStyles.write(mergedStyles[i]);
+              }
+            }
+          );
+        }
+      }
+    });
+  }
+});
 
 // fs.mkdir(
 //   path.join(__dirname, "project-dist", "assets"),
