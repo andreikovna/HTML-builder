23de2fd1e64c719c4bf479d61b59e856588a6c74
Daria Litvinova <d.andreikovna@gmail.com>
2021-11-02 12:51:59 +0300

    refactor: formatting, check 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,56 +1,50 @@
-const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
+const path = require("path");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
 
-const destination = path.join(__dirname, 'files-copy');
-const source = path.join(__dirname, 'files');
+const destination = path.join(__dirname, "files-copy");
+const source = path.join(__dirname, "files");
 
-fs.access(destination, function(error){
-    if (error) {
-        copyFiles()
-    } else {
-        deleteAndCopy()
+fs.access(destination, function (error) {
+  if (error) {
+    copyFiles();
+  } else {
+    deleteAndCopy();
+  }
+});
+
+async function deleteAndCopy() {
+  await fsPromises.rmdir(destination, { recursive: true }, (err) => {
+    if (err) {
+      return console.error(err);
     }
-})
-
-async function deleteAndCopy(){
-    await fsPromises.rmdir(destination, { recursive: true }, (err) => {
-        if (err) {
-            return console.error(err);
-          }
-    } )
-    copyFiles()
-    
+  });
+  copyFiles();
 }
 
-function copyFiles () {
-    fs.mkdir(destination, { recursive: true }, (err) => {
-        if (err) {
-          return console.error(err);
-        }
-        fs.readdir(source, {withFileTypes: true}, (err, files) => {
-            if (err) {
-                console.log(err);
-            } else {
-                files.forEach(file => {
-                    const fileName = file.name.toString();
-              
-                    fsPromises.copyFile(path.join(__dirname, 'files', fileName),
-                                      path.join(__dirname, 'files-copy', fileName),)
-                      .catch(function(error) {
-                          console.log(error);
-                      })
-                })
-                console.log('Folder Copied');
-            }
-        })
-      });
+function copyFiles() {
+  fs.mkdir(destination, { recursive: true }, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    fs.readdir(source, { withFileTypes: true }, (err, files) => {
+      if (err) {
+        console.log(err);
+      } else {
+        files.forEach((file) => {
+          const fileName = file.name.toString();
+
+          fsPromises
+            .copyFile(
+              path.join(__dirname, "files", fileName),
+              path.join(__dirname, "files-copy", fileName)
+            )
+            .catch(function (error) {
+              console.log(error);
+            });
+        });
+        console.log("Folder Copied");
+      }
+    });
+  });
 }
-
-
-
-
-
-  
-  
-
