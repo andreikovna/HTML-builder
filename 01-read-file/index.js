70daf5e18fb2969a6d812035f528c05df2a63775
Daria Litvinova <d.andreikovna@gmail.com>
2021-11-02 12:08:33 +0300

    refactor: formatting 01-read-file, check productivity

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,17 +1,3 @@
-// const { stdin, stdout, exit} = process;
-// const flag = process.argv[2];
-// const allowedFlags = ['-f', '-d'];
-// if (!allowedFlags.includes(flag)) {
-//     stdout.write('Попробуйте ещё раз запустить файл с флагом -f или -d');
-//     exit();
-// }
-// if (flag == '-d') {
-//     console.log(__dirname);
-// }
-// if (flag == '-f'){
-//     console.log(__filename);
-// }
-
 const path = require('path');
 const fs = require('fs');
 const {stdout} = process;
