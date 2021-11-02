9f2adda79a8fc68903652f33f4bfce03025bb655
Daria Litvinova <d.andreikovna@gmail.com>
2021-11-02 12:27:02 +0300

    refactor: check working productivity, formatting 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,22 +1,22 @@
-const path = require('path');
-const fs = require('fs');
-const {stdin, stdout} = process;
+const path = require("path");
+const fs = require("fs");
+const { stdin, stdout } = process;
 
-const resultText = path.join(__dirname, 'text.txt');
+const resultText = path.join(__dirname, "text.txt");
 const output = fs.createWriteStream(resultText);
 
-console.log ('\nplease, write smth here:');
-stdin.on('data', data => {
-    const receivedData = data.toString().trim();
-    if (receivedData == 'exit') {
-        process.exit();
-    } else {
-        output.write(data); 
-        console.log('\nyou can add smth or write "exit"')
-    }  
+console.log("\nplease, write smth here:");
+stdin.on("data", (data) => {
+  const receivedData = data.toString().trim();
+  if (receivedData == "exit") {
+    process.exit();
+  } else {
+    output.write(data);
+    console.log('\nyou can add smth or write "exit"');
+  }
 });
 
-process.on('SIGINT', function () {
-    process.exit();
-  });
-process.on('exit', () => stdout.write('\ngood luck!'));
+process.on("SIGINT", function () {
+  process.exit();
+});
+process.on("exit", () => stdout.write("\ngood luck!!"));
